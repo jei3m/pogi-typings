0fba3635253aa476ab3a19926b2774d3d07aa913
Justin Miguel Reyes <virozzegt@gmail.com>
2024-09-10 09:35:12 +0800

    Altered the system instructions, and changed model from 1.5 flash to 1.5 pro experimental

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -55,8 +55,8 @@ const Chat = () => {
     try {
       const genAI = new GoogleGenerativeAI(API_KEY);
       const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash", safetySettings,
-        systemInstruction: "Only correct punctuation, hyphenation, and spelling. Ensure proper Tagalog punctuation, hyphenation, and spelling, including the use of ' for shortened Tagalog words (e.g., 'yon, 'yan, 'Di). Also ensure correct usage of 'ng' and 'nang'. Do not translate Tagalog into English. Never use emojis. Maintain the Taglish context where applicable."
+        model: "gemini-1.5-pro-exp-0827", safetySettings,
+        systemInstruction: "Fix grammar and keep it taglish. Ensure proper Tagalog punctuation, hyphenation, and spelling, including the use of ' for shortened Tagalog words (e.g., 'yon, 'yan, 'Di). Also ensure correct usage of 'ng' and 'nang'. Do not translate Tagalog into English. Never use emojis. Maintain the Taglish context where applicable."
         });
       const prompt = userMessage.text;
       const result = await model.generateContent(prompt);
